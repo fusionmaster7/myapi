53c732750a84bd5effeb524c1b2d11cb05757d09
Hardik Singh <hardiksingh297@gmail.com>
2020-01-04 01:08:31 +0530

    Created a basic GET route for the api and tested using Postman.

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-//Set static folder
+
+const members = [
+  {
+    id: 1,
+    name: 'Hardik',
+    status: 'active'
+  },
+  {
+    id: 2,
+    name: 'Soumya',
+    status: 'active'
+  },
+  {
+    id: 3,
+    name: 'Seema',
+    status: 'inactive'
+  }
+];
+
+app.get('/api/members',function(req,res){
+  res.json(members);
+});
+
 app.use(express.static(path.join(__dirname,'public')));
+
 app.listen(3000);
